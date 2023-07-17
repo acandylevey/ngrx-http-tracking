b7ac7de83f273b60455941366543e463a3c437e6
David Elcheikh <delcheikh@campion.com.au>
2023-07-17 16:05:46 +1000

    fix peer deps

--- ngrx-http-tracking/projects/ngrx-http-tracking/src/public-api.ts
+++ ngrx-http-tracking/projects/ngrx-http-tracking/src/public-api.ts
@@ -6,3 +6,4 @@ export * from './lib/+state/http-tracking.facade';
 export * from './lib/function/http-tracking-actions.factory';
 export * from './lib/http-tracking.module';
 export * from './lib/model/http-tracking-entity';
+export * from './lib/function/http-tracking-filter';
