7b92fdc6d7328dc650c9e6c4412fd884cfa8b5f5
Fabio Lima <flima@campion.com.au>
2026-05-12 12:37:46 +1000

    add comments

--- ngrx-http-tracking/projects/ngrx-http-tracking/src/lib/function/convert-response-to-error.ts
+++ ngrx-http-tracking/projects/ngrx-http-tracking/src/lib/function/convert-response-to-error.ts
@@ -2,6 +2,10 @@ export const convertResponseToError = (err: any, fallbackMessage: string) => {
     let errorMsg = '';
     if (err?.name === 'HttpErrorResponse' && typeof err.error === 'string') {
         try {
+            // When endpoints yield NoValue as a success response, the openApi files we
+            // auto-generate would document the responses to that endpoint with "text" type.
+            // Because of that, angular would place the JSON as text into the error here.
+            // We need to counter it by attempting to parse the error msg:
             const parsed = JSON.parse(err.error);
             errorMsg = typeof parsed?.error === 'string' ? parsed.error : err.error;
         } catch {
